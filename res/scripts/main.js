b7a1df6b8280024a897d707e52ec47a9f4a6dc99
Krishna Kumar <71817691+krishna8421@users.noreply.github.com>
2021-10-10 22:55:09 +0530

    Revert Double quotes to single || Add .prettierrc file for 4 space Indentation

--- res/scripts/main.js
+++ res/scripts/main.js
@@ -1,163 +1,154 @@
-let rootNode = document.getElementById("root");
+let rootNode = document.getElementById('root');
 let users = new Set();
 let loadingFinished = false;
 
-let menuRoot = document.getElementById("menu-bar");
+let menuRoot = document.getElementById('menu-bar');
 
 window.onload = () => {
-  users = new Set(JSON.parse(localStorage.getItem("usernames")));
-  if (users.size === 0) {
-    toggleMenu();
-  }
-  for (let user of users) {
-    getDetails(user);
-  }
-  loadingFinished = true;
-};
+    users = new Set(JSON.parse(localStorage.getItem('usernames')));
+    if (users.size === 0 ) {toggleMenu();}
+    for (let user of users){
+            getDetails(user);
+    }
+    loadingFinished = true;
+}
 
 function addUser() {
-  let username = document.getElementById("username").value;
-  document.getElementById("username").value = "";
-  if (username === "") return;
-  getDetails(username);
-  toggleMenu();
+    let username = document.getElementById('username').value;
+    document.getElementById('username').value = '';
+    if (username === '') return;
+    getDetails(username);
+    toggleMenu();
 }
 // Trigger add using Enter key
-window.addEventListener("keypress", function (e) {
-  if (e.key === "Enter") {
-    document.querySelector("#menu-content button").click();
-  }
+window.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        document.querySelector("#menu-content button").click();
+    }
 });
 
+
 function deleteUser() {
-  const card = this.parentElement.parentElement;
-  const username = card
-    .getElementsByClassName("username")[0]
-    .innerHTML.slice(1, -1);
-  users.delete(username);
-  storeData(users);
-  this.parentElement.parentElement.remove();
-  users = new Set(JSON.parse(localStorage.getItem("usernames")));
-  if (users.size === 0) {
-    toggleMenu();
-  }
+    const card = this.parentElement.parentElement;
+    const username = card.getElementsByClassName('username')[0].innerHTML.slice(1,-1);
+    users.delete(username);
+    storeData(users);
+    this.parentElement.parentElement.remove();
+    users = new Set(JSON.parse(localStorage.getItem('usernames')));
+    if (users.size === 0 ) {toggleMenu();}
 }
 
 function storeData(data) {
-  localStorage.setItem("usernames", JSON.stringify(Array.from(data)));
+    localStorage.setItem('usernames',JSON.stringify(Array.from(data)));
 }
 
-function getDetails(username) {
-  username = username.toLowerCase();
-  if (users.has(username) && loadingFinished) {
-    alert(`Username "${username}" already exists.`);
-    return;
-  }
-  fetch(`https://competitive-coding-api.herokuapp.com/api/codechef/${username}`)
-    .then(function (result) {
-      return result.json();
-    })
-    .then(function (data) {
-      // console.log(data);
-      if (data.status === "Failed" && data.details === "Invalid username") {
-        alert(`"${username}" is an invalid username.`);
+function getDetails(username){
+    username = username.toLowerCase();
+    if (users.has(username) && loadingFinished) {
+        alert(`Username "${username}" already exists.`);
         return;
-      }
-      const information = {
-        name: data.user_details.name,
-        username: data.user_details.username,
-        rating: data.rating,
-        highestRating: data.highest_rating,
-        stars: data.stars,
-        countryRank: data.country_rank,
-        globalRank: data.global_rank,
-      };
-      createCard(information);
-      users.add(username);
-      storeData(users);
-    })
-    .catch((error) => console.error("Cannot Fetch Data From API, ", error));
-}
-
-function createCard({
-  name,
-  username,
-  rating,
-  highestRating,
-  stars,
-  countryRank,
-  globalRank,
-}) {
-  const root = document.createElement("div");
-  root.classList.add("card");
-
-  const div_name = document.createElement("div");
-  div_name.classList.add("card-header");
-
-  const e_name = document.createElement("p");
-  e_name.innerHTML = name;
-  e_name.classList.add("name");
-
-  const e_username = document.createElement("p");
-  e_username.innerHTML = `(${username})`;
-  e_username.classList.add("username");
-
-  div_name.appendChild(e_name);
-  div_name.appendChild(e_username);
-
-  const e_rating = document.createElement("p");
-  e_rating.innerHTML = `Rating: ${rating} (${stars})`;
-  e_rating.classList.add("rating");
-
-  const div_others = document.createElement("div");
-  div_others.classList.add("others");
-
-  const e_highestRating = document.createElement("p");
-  e_highestRating.innerHTML = `Highest Rating: ${highestRating}`;
-  e_highestRating.classList.add("highest-rating");
-
-  const e_globalRank = document.createElement("p");
-  e_globalRank.innerHTML = `Global Rank: ${globalRank}`;
-
-  const e_countryRank = document.createElement("p");
-  e_countryRank.innerHTML = `Country Rank: ${countryRank}`;
-
-  const e_delete = document.createElement("button");
-  e_delete.innerHTML = "Delete";
-  e_delete.classList.add("delete-button");
-  e_delete.addEventListener("click", deleteUser);
-
-  div_others.appendChild(e_highestRating);
-  div_others.appendChild(e_globalRank);
-  div_others.appendChild(e_countryRank);
-  div_others.appendChild(e_delete);
-
-  root.appendChild(div_name);
-  root.appendChild(e_rating);
-  root.appendChild(div_others);
-
-  // Sorting happens here
-  for (let currentNode of rootNode.childNodes) {
-    const ratingElement = currentNode.querySelector(".rating");
-    const currentNodeRating = parseInt(ratingElement.innerHTML.split(" ")[1]);
-    if (rating >= currentNodeRating) {
-      rootNode.insertBefore(root, currentNode);
-      return;
     }
-  }
-  // If we get here, all other nodes have lower values or it is the first node
-  rootNode.appendChild(root);
+    fetch(`https://competitive-coding-api.herokuapp.com/api/codechef/${username}`)
+    .then( function (result) { return result.json() } )
+    .then(
+        function (data) {
+            // console.log(data);
+            if(data.status === 'Failed' && data.details === 'Invalid username'){
+                alert(`"${username}" is an invalid username.`);
+                return;
+            }
+            const information = {
+                name: data.user_details.name,
+                username: data.user_details.username,
+                rating: data.rating,
+                highestRating: data.highest_rating,
+                stars: data.stars,
+                countryRank: data.country_rank,
+                globalRank: data.global_rank 
+            }
+            createCard(information);
+            users.add(username);
+            storeData(users);
+        }
+    )
+    .catch((error) => console.error("Cannot Fetch Data From API, ",error))
 }
 
-function toggleMenu() {
-  const toggle_icon = menuRoot.children[0].getElementsByTagName("i")[0];
-  const menu_content = menuRoot.children[1];
-  if (toggle_icon.classList.contains("fa-chevron-up")) {
-    toggle_icon.classList.remove("fa-chevron-up");
-    toggle_icon.classList.add("fa-chevron-down");
-  } else {
-    toggle_icon.classList.remove("fa-chevron-down");
-    toggle_icon.classList.add("fa-chevron-up");
-  }
-  menu_content.classList.toggle("hide");
+function createCard({name, username, rating, highestRating, stars, countryRank, globalRank}){
+
+    const root = document.createElement("div");
+    root.classList.add("card");
+
+    const div_name = document.createElement("div"); 
+    div_name.classList.add("card-header");
+
+    const e_name = document.createElement("p");
+    e_name.innerHTML = name;
+    e_name.classList.add("name");
+
+    const e_username = document.createElement("p");
+    e_username.innerHTML = `(${username})`;
+    e_username.classList.add("username");
+    
+    div_name.appendChild(e_name);
+    div_name.appendChild(e_username);
+
+    const e_rating = document.createElement("p");
+    e_rating.innerHTML = `Rating: ${rating} (${stars})`;
+    e_rating.classList.add("rating");
+
+    const div_others = document.createElement("div"); 
+    div_others.classList.add("others");
+    
+    const e_highestRating = document.createElement("p");
+    e_highestRating.innerHTML = `Highest Rating: ${highestRating}`;
+    e_highestRating.classList.add("highest-rating");
+    
+    const e_globalRank = document.createElement("p");
+    e_globalRank.innerHTML = `Global Rank: ${globalRank}`;
+
+    const e_countryRank = document.createElement("p");
+    e_countryRank.innerHTML = `Country Rank: ${countryRank}`;
+
+    const e_delete = document.createElement("button");
+    e_delete.innerHTML = 'Delete';
+    e_delete.classList.add('delete-button')
+    e_delete.addEventListener('click', deleteUser);
+
+    div_others.appendChild(e_highestRating);
+    div_others.appendChild(e_globalRank);
+    div_others.appendChild(e_countryRank);
+    div_others.appendChild(e_delete);
+
+
+    root.appendChild(div_name);
+    root.appendChild(e_rating);
+    root.appendChild(div_others);
+    
+    // Sorting happens here
+    for (let currentNode of rootNode.childNodes){
+        const ratingElement = currentNode.querySelector(".rating");
+        const currentNodeRating = parseInt(ratingElement.innerHTML.split(" ")[1]);
+        if (rating >= currentNodeRating){
+            rootNode.insertBefore(root,currentNode);
+            return;
+        }
+    }
+    // If we get here, all other nodes have lower values or it is the first node
+    rootNode.appendChild(root);
+}
+
+
+function toggleMenu()
+{
+    const toggle_icon = menuRoot.children[0].getElementsByTagName('i')[0];
+    const menu_content = menuRoot.children[1];
+    if(toggle_icon.classList.contains('fa-chevron-up')) {
+        toggle_icon.classList.remove('fa-chevron-up');
+        toggle_icon.classList.add('fa-chevron-down');
+    } else {
+        toggle_icon.classList.remove('fa-chevron-down');
+        toggle_icon.classList.add('fa-chevron-up');   
+    }
+    menu_content.classList.toggle('hide');
 }
