ddfb2ac1c5b07443de8e95b567f718211b8339c7
Nitin Nair <nitin.dreamern89@gmail.com>
2021-11-03 20:50:59 +0530

    Added useEffect hook to check device resolution and update state.

--- src/Card.js
+++ src/Card.js
@@ -1,98 +1,115 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Card.css";
 import { FaUserTimes } from "react-icons/fa";
 
 export default function Card({ user_data, setShowModal, removeUser }) {
-    const {
-        user_details,
-        stars,
-        rating,
-        highest_rating,
-        global_rank,
-        country_rank,
-        partially_solved,
-        fully_solved,
-    } = user_data;
+	const {
+		user_details,
+		stars,
+		rating,
+		highest_rating,
+		global_rank,
+		country_rank,
+		partially_solved,
+		fully_solved,
+	} = user_data;
 
-    // To Give Accent Color To Card.
-    const colorClass = `stars${stars[0]}`;
+	// To Give Accent Color To Card.
+	const colorClass = `stars${stars[0]}`;
 
-    // To show/hide more details in small screens
-    const [isMoreDetailsShown, setMoreDetailsShown] = useState("");
-    const fnToggleMoreDetails = () => {
-        setMoreDetailsShown((prev) => !prev);
-    };
+	// To show/hide more details in small screens
+	const [isMoreDetailsShown, setMoreDetailsShown] = useState("");
+	const [width, setWidth] = useState(window.innerWidth);
 
-    return (
-        <div className={`cardBox ${colorClass}`}>
-            <div className="cardDetails">
-                <h1>{user_details.name}</h1>
-                <p>({user_details.username})</p>
-                <div className="cardRating">
-                    <p>
-                        {stars} | {rating}
-                    </p>
-                </div>
-                <div className="cardHighestRating">
-                    <h4>Highest Rating:</h4>
-                    <p>{highest_rating}</p>
-                </div>
-                <div>
-                    <button
-                        className="cardBtn"
-                        onClick={() => {
-                            setShowModal((previous) => {
-                                return {
-                                    ...previous,
-                                    visible: true,
-                                    type: "confirm",
-                                    msg: `Are you sure to delete '${user_details.username}'?`,
-                                    acceptFunc: () =>
-                                        removeUser(user_details.username),
-                                };
-                            });
-                        }}
-                    >
-                        <FaUserTimes />
-                        <p>Delete</p>
-                    </button>
-                </div>
-                <button
-                    id="btnViewMoreDetails"
-                    type="button"
-                    className="cardBtn"
-                    onClick={fnToggleMoreDetails}
-                >
-                    <span>{isMoreDetailsShown ? "Close" : "View"} details</span>
-                </button>
-            </div>
+	function handleWindowSizeChange() {
+		setWidth(window.innerWidth);
+	}
 
-            {isMoreDetailsShown && (
-                <div className="moreDetails">
-                    <div className="detailsItemWrapper">
-                        <h1 className="wrapperHeading">Ranking</h1>
-                        <div className="detailsItem">
-                            <p>Global Rank:</p>
-                            <p>{global_rank}</p>
-                        </div>
-                        <div className="detailsItem">
-                            <p>Country Rank:</p>
-                            <p>{country_rank}</p>
-                        </div>
-                    </div>
-                    <div className="detailsItemWrapper">
-                        <h1 className="wrapperHeading">Problem Solved</h1>
-                        <div className="detailsItem">
-                            <p>Fully:</p>
-                            <p>{fully_solved.count}</p>
-                        </div>
-                        <div className="detailsItem">
-                            <p>Partially:</p>
-                            <p>{partially_solved.count}</p>
-                        </div>
-                    </div>
-                </div>
-            )}
-        </div>
-    );
+	useEffect(() => {
+		window.addEventListener("resize", handleWindowSizeChange);
+		return () => {
+			window.removeEventListener("resize", handleWindowSizeChange);
+		};
+	}, []);
+
+	const isDeviceMobile = width <= 768;
+
+	const fnToggleMoreDetails = () => {
+		setMoreDetailsShown((prev) => !prev);
+	};
+
+	return (
+		<div className={`cardBox ${colorClass}`}>
+			<div className="cardDetails">
+				<h1>{user_details.name}</h1>
+				<p>({user_details.username})</p>
+				<div className="cardRating">
+					<p>
+						{stars} | {rating}
+					</p>
+				</div>
+				<div className="cardHighestRating">
+					<h4>Highest Rating:</h4>
+					<p>{highest_rating}</p>
+				</div>
+				<div>
+					<button
+						className="cardBtn"
+						onClick={() => {
+							setShowModal((previous) => {
+								return {
+									...previous,
+									visible: true,
+									type: "confirm",
+									msg: `Are you sure to delete '${user_details.username}'?`,
+									acceptFunc: () => removeUser(user_details.username),
+								};
+							});
+						}}
+					>
+						<FaUserTimes />
+						<p>Delete</p>
+					</button>
+				</div>
+
+				{isDeviceMobile && (
+					<button
+						id="btnViewMoreDetails"
+						type="button"
+						className="cardBtn"
+						onClick={fnToggleMoreDetails}
+					>
+						<span>{isMoreDetailsShown ? "Close" : "View"} details</span>
+					</button>
+				)}
+			</div>
+
+			{(isMoreDetailsShown || !isDeviceMobile) && (
+				<div className="moreDetails">
+					<div className="detailsItemWrapper">
+						<h1 className="wrapperHeading">Ranking</h1>
+						<div className="detailsItem">
+							<p>Global Rank:</p>
+							<p>{global_rank}</p>
+						</div>
+						<div className="detailsItem">
+							<p>Country Rank:</p>
+							<p>{country_rank}</p>
+						</div>
+					</div>
+					<div className="detailsItemWrapper">
+						<h1 className="wrapperHeading">Problem Solved</h1>
+						<div className="detailsItem">
+							<p>Fully:</p>
+							<p>{fully_solved.count}</p>
+						</div>
+						<div className="detailsItem">
+							<p>Partially:</p>
+							<p>{partially_solved.count}</p>
+						</div>
+					</div>
+				</div>
+			)}
+		</div>
+	);
 }
